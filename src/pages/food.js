4794904984e6346ee444378b0b7840be52877c95
Diane So <so.dianeyj@gmail.com>
2021-07-21 17:22:23 -0400

    implemented two different types of carousels

--- src/pages/food.js
+++ src/pages/food.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from 'react';
+import Carousel, { slidesToShowPlugin } from "@brainhubeu/react-carousel";
+import "@brainhubeu/react-carousel/lib/style.css";
+import * as styles from "../styles/images.css";
 
 export default function food() {
   return (
-    <div>
+    <div className="food-container">
       <h2>Food</h2>
+      <div className="food-img-carousel-container">
+        <Carousel
+          plugins={[
+            "clickToChange",
+            "centered",
+            {
+              resolve: slidesToShowPlugin,
+              options: {
+                numberOfSlides: 3,
+              },
+            },
+          ]}
+        >
+          <img src="img1.png" />
+          <img src="img2.png" />
+          <img src="img3.png" />
+        </Carousel>
+      </div>
     </div>
   )
 }
