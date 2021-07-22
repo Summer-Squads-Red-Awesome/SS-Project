3fbcae53f325f70f30ceb5e79e1bed14e21c4987
Diane So <so.dianeyj@gmail.com>
2021-07-22 19:04:51 -0400

    added layout to food.js

--- src/pages/food.js
+++ src/pages/food.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import Carousel, { slidesToShowPlugin } from "@brainhubeu/react-carousel";
+import Layout from "../components/Layout";
 import "@brainhubeu/react-carousel/lib/style.css";
 import * as styles from "../styles/images.css";
 
 export default function food() {
   return (
-    <div className="food-container">
-      <h2>Food</h2>
-      <div className="food-img-carousel-container">
-        <Carousel
-          plugins={[
-            "clickToChange",
-            "centered",
-            {
-              resolve: slidesToShowPlugin,
-              options: {
-                numberOfSlides: 3,
+    <Layout>
+      <div className="food-container">
+        <h2>Food</h2>
+        <div className="food-img-carousel-container">
+          <Carousel
+            plugins={[
+              "clickToChange",
+              "centered",
+              {
+                resolve: slidesToShowPlugin,
+                options: {
+                  numberOfSlides: 3,
+                },
               },
-            },
-          ]}
-        >
-          <img src="img1.png" />
-          <img src="img2.png" />
-          <img src="img3.png" />
-        </Carousel>
+            ]}
+          >
+            <img src="img1.png" />
+            <img src="img2.png" />
+            <img src="img3.png" />
+          </Carousel>
+        </div>
       </div>
-    </div>
+    </Layout>
   )
 }
