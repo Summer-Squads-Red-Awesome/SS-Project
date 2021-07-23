704ac6d892a7fa295c6f1c3513e79c9a7f61fca9
Diane So <so.dianeyj@gmail.com>
2021-07-23 11:11:11 -0400

    more small adjustments to navbar, changed home to ecoviz

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <nav>
       <div className="nav-links-container">
-        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/" className="nav-link" id="ecoviz">EcoViz</Link>
         <Link to="/visualizations" className="nav-link">Visualizations</Link>
         <Link to="/resources" className="nav-link">Resources</Link>
       </div>
