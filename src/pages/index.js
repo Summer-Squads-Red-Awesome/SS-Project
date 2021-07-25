26f7c673c5150724c47e1d71b18c618f1a9b52bf
Diane So <so.dianeyj@gmail.com>
2021-07-25 13:55:00 -0400

    added cover images to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,21 +9,41 @@ export default function Home() {
       <div className="homescreen">
         <div className="link-container">
           <Link classname="home-link" to="/commute">
+            <img
+              src="fuel-cover.jpg"
+              alt="The Daily Commute"
+              className="cover-image"
+            />
             The Daily Commute
           </Link>
         </div>
         <div className="link-container">
           <Link classname="home-link" to="/trash">
+            <img
+              src="trash-cover.jpg"
+              alt="Wait, how much trash?"
+              className="cover-image"
+            />
             Wait, How Much Trash?
           </Link>
         </div>
         <div className="link-container">
           <Link classname="home-link" to="/diet">
+            <img
+              src="diet-cover.jpg"
+              alt="A carnivore diet"
+              className="cover-image"
+            />
             A Carnivore Diet
           </Link>
         </div>
         <div className="link-container">
           <Link classname="home-link" to="/lights">
+            <img
+              src="lights-cover.jpg"
+              alt="All the lights"
+              className="cover-image"
+            />
             All the Lights
           </Link>
         </div>
