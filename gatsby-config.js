f5e6b6da0f9f555e2cf65b47fa8c7595edd8b49f
Diane So <so.dianeyj@gmail.com>
2021-07-25 17:36:25 -0400

    some more styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,19 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: 'EcoViz'
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `limelight`,
+          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      }
+    }
+  ],
 }
