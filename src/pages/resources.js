6bbc010fb55dba5c100acacb373b0f9acc765796
Thay <pattasiwa@gmail.com>
2021-07-26 05:44:46 -0400

    footer, index styling

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,10 +1,11 @@
-import React from "react";
-import Layout from "../components/Layout";
+import React from "react"
+import Layout from "../components/Layout"
+import "../styles/resources.css"
 
 export default function resources() {
   return (
     <Layout>
-      <div>
+      <div className="resources-container">
         <h2>Resources</h2>
       </div>
     </Layout>
--- src/pages/visualizations.js
+++ src/pages/visualizations.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
+import "../styles/visualizations.css"
 
 export default function visualizations() {
   return (
     <Layout>
-      <div>
+      <div className="visualization-container">
         <h2>Visualizations</h2>
       </div>
     </Layout>
